08f35e29be82131b4a2ee77319f5b9041be8f351
linc <qulamj@gmail.com>
2022-01-14 14:42:30 +0800

    fix: octane 啟動前檢查上次啟動殘存的資料

---
+++ 8.0/optional/octane/octane-dev.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+php /var/www/artisan octane:stop >/dev/null
+rm -f /var/www/storage/framework/octane/octane-server-state.json /var/www/storage/logs/octane-server-state.json
+php /var/www/artisan octane:start --watch
